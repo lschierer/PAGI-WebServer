49c5e5cb8c64adb8d965d63b91259a3a8178c3c4
Luke Schierer <luke@schierer.org>
2026-02-09 01:00:52 -0500

    add in a project specific root script step

--- lib/Infrastructure/prefixBin/rootBootstrap.sh
+++ lib/Infrastructure/prefixBin/rootBootstrap.sh
@@ -20,58 +20,63 @@ HELP_FLAG=0
 
 # Loop through the arguments
 while true; do
-    case "$1" in
-        -m | --mode)
-            MODE_VALUE="$2"
-            shift 2 # Shift past the option and the argument
-            ;;
-        -d | --domain1)
-            DOMAIN1="$2"
-            shift 2
-            ;;
-        -h | --help)
-            HELP_FLAG=1
-            shift # Shift past the option
-            ;;
-        --) # End of options marker
-            shift
-            break
-            ;;
-        *)
-            echo "Internal error: unrecognized option $1" >&2
-            exit 1
-            ;;
-    esac
+  case "$1" in
+  -m | --mode)
+    MODE_VALUE="$2"
+    shift 2 # Shift past the option and the argument
+    ;;
+  -d | --domain1)
+    DOMAIN1="$2"
+    shift 2
+    ;;
+  -h | --help)
+    HELP_FLAG=1
+    shift # Shift past the option
+    ;;
+  --) # End of options marker
+    shift
+    break
+    ;;
+  *)
+    echo "Internal error: unrecognized option $1" >&2
+    exit 1
+    ;;
+  esac
 done
 
 # Function to display help information
 display_help() {
-    echo "Usage: $0 [-m <mode> | --mode=<mode>]"
-    echo "Options:"
-    echo "  -m, --mode  Specify the operation mode (e.g., 'read', 'write')"
-    echo "  -d, --domain1 Specify the domain name for the primary domain"
-    echo "  -h, --help  Display this help message"
+  echo "Usage: $0 [-m <mode> | --mode=<mode>]"
+  echo "Options:"
+  echo "  -m, --mode  Specify the operation mode (e.g., 'read', 'write')"
+  echo "  -d, --domain1 Specify the domain name for the primary domain"
+  echo "  -h, --help  Display this help message"
 }
 
 # Post-processing to ensure the 'mode' parameter was provided
 if [ $HELP_FLAG -eq 1 ]; then
-    display_help
-    exit 0
+  display_help
+  exit 0
 fi
 
 if [ -z "$MODE_VALUE" ]; then
-    echo "Error: The 'mode' parameter is required." >&2
-    display_help >&2
-    exit 1
+  echo "Error: The 'mode' parameter is required." >&2
+  display_help >&2
+  exit 1
 fi
 
 if [ -z "$DOMAIN1" ]; then
-    echo "Error: The 'domain1 parameter is required." >&2
-    display_help >&2
-    exit 1
+  echo "Error: The 'domain1 parameter is required." >&2
+  display_help >&2
+  exit 1
 fi
 
 /opt/prefix/bin/setup-cert.sh --mode ${MODE_VALUE} --domain1 ${DOMAIN1} >> /var/log/setup-cert.log 2>&1 
 /opt/prefix/bin/setup-nginx.sh --mode ${MODE_VALUE} --domain1 ${DOMAIN1} >> /var/log/setup-nginx.log 2>&1
+
+if [ -f /opt/prefix/app/scripts/root_prereq_steps.sh ]; then
+  chmod 755 /opt/prefix/app/scripts/root_prereq_steps.sh
+  /opt/prefix/app/scripts/root_prereq_steps.sh --mode ${MODE_VALUE} --domain1 ${DOMAIN1} >> /var/log/root_preqreq_steps.log
+fi
 
 echo "root bootstrap complete";
