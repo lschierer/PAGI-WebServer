420352c47ceb390f586dc5e3434bc8f3dcd375e1
Luke Schierer <luke@schierer.org>
2026-02-22 13:59:24 -0500

    these need to be global, not EvonyTKR specific

--- lib/WebFramework/Controller/Base.pm
+++ lib/WebFramework/Controller/Base.pm
@@ -7,6 +7,7 @@ extends 'Thunderhorse::Controller';
 with 'WebFramework::Role::Markdown';
 with 'WebFramework::Role::LogConfig';
 
+require JSON::MaybeXS;
 use Future::AsyncAwait;
 use Path::Tiny;
 use Encode      qw(encode_utf8);
@@ -22,5 +23,87 @@ has app_config => (
 
 sub build ($self) {
   $self->SUPER::build();
+
+  # Robots.txt route
+  $router->add(
+    '/robots.txt',
+    {
+      to => sub ($self, $ctx) {
+        my $host   = $ctx->req->headers->{'host'} // '';
+        my $is_dev = $host =~ /dev|localhost|127\.0\.0\.1/i;
+
+        my $base   = $ctx->req->scheme . '://' . $ctx->req->host . '/';
+        my $robots =
+          $is_dev
+          ? "User-agent: *\nDisallow: /\n"
+          : "User-agent: *\nDisallow:\nSitemap: "
+          . $base
+          . "sitemap.xml\n";
+
+        $ctx->res->headers(content_type => 'text/plain');
+        return $robots;
+      },
+      action => 'http.*',
+    }
+  );
+
+  # Health check route
+  $router->add(
+    '/health',
+    {
+      to => sub ($self, $ctx) {
+        my $APP_START_TIME = $self->app->config->{config}->{APP_START_TIME}
+          // time();
+        
+        # Determine if we're in EC2 or container environment
+        my $is_ec2 = !$deployment_env->{'IMAGE_TAG'};
+
+        my $env_info = {};
+        if ($is_ec2) {
+          # EC2 deployment info
+          $env_info = {
+            deployment_type => 'ec2',
+            hostname        => $deployment_env->{'HOSTNAME'} // `hostname`,
+            git_commit      =>
+              $self->app->config->{config}->{version}->{'git-commit'}
+              // 'unknown',
+            git_branch =>
+              $self->app->config->{config}->{version}->{'git-branch'}
+              // 'unknown',
+            build_time =>
+              $self->app->config->{config}->{version}->{'build-time'}
+              // 'unknown',
+            cdk_deployment_time => $deployment_env->{'DEPLOYMENT_TIME'}
+              // 'unknown',
+          };
+          chomp $env_info->{hostname} if $env_info->{hostname};
+        }
+        else {
+          # Container deployment info
+          $env_info = {
+            deployment_type     => 'container',
+            cdk_deployment_time => $deployment_env->{'DEPLOYMENT_TIME'}
+              // 'unknown',
+          };
+        }
+        
+        my $json = JSON::MaybeXS->new(utf8 => 1, pretty => 1);
+
+        my $response = $json->encode({
+          status             => 'ok',
+          mode               => $self->app->env          // 'unknown',
+          version            => $self->app->config->{config}->{version} // 'unknown',
+          time               => scalar localtime,
+          app_started_at     => scalar(localtime($APP_START_TIME)),
+          app_uptime_seconds => time() - $APP_START_TIME,
+          %$env_info,
+        });
+
+        $ctx->res->headers(content_type => 'application/json; charset=utf-8');
+        return $response;
+      },
+      action => 'http.*',
+    }
+  );
 
 }
